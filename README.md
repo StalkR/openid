c97910b90d1302677f611f37cc95816e0472c4e4
StalkR <stalkr@stalkr.net>
2019-12-31 13:24:55 +0100

    openid: add package

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go OpenID Connect authentication library
+
+[![Build Status][1]][2] [![Godoc][3]][4]
+
+Bugs, comments, questions: create a [new issue][5].
+
+[1]: https://api.travis-ci.org/StalkR/openid.png?branch=master
+[2]: https://travis-ci.org/StalkR/openid
+[3]: https://godoc.org/github.com/StalkR/openid?status.png
+[4]: https://godoc.org/github.com/StalkR/openid
+[5]: https://github.com/StalkR/openid/issues/new
---
+++ example_test.go
@@ -0,0 +1,23 @@
+package openid
+
+import (
+        "context"
+        "fmt"
+        "net/http"
+)
+
+func ExampleAuth() {
+        ctx := context.Background()
+        auth := openid.New(ctx, &openid.Config{
+                Provider: "https://accounts.google.com",
+                ClientID: "xxx.apps.googleusercontent.com",
+        })
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                user, err := auth.User(r)
+                if err != nil {
+                        auth.Redirect(w, r)
+                        return
+                }
+                fmt.Fprintf(w, "Hello %v", user)
+        })
+}
