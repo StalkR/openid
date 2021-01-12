eff8d395ebd17d1cc53a5c0296a8fc0220ff056e
StalkR <stalkr@stalkr.net>
2021-01-12 14:37:35 +0100

    update go-oidc v2 (broken) to v3

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/StalkR/openid
 go 1.13
 
 require (
-	github.com/coreos/go-oidc v2.1.0+incompatible
-	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876 // indirect
-	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
-	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
+	github.com/coreos/go-oidc/v3 v3.0.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- openid.go
+++ openid.go
@@ -47,22 +47,22 @@ To use it:
 package openid
 
 import (
-    "context"
-    "crypto/rand"
-    "encoding/hex"
-    "fmt"
-    "log"
-    "net/http"
-    "net/url"
-    "strings"
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
-    oidc "github.com/coreos/go-oidc"
+	"github.com/coreos/go-oidc/v3/oidc"
 )
 
 // Config configures the auth module.
 type Config struct {
-    Provider string
-    ClientID string
+	Provider string
+	ClientID string
 }
 
 const callback = "/auth/callback"
@@ -70,58 +70,58 @@ const callback = "/auth/callback"
 // New creates a new authentication module.
 // It registers a handler at /auth/callback for the provider.
 func New(ctx context.Context, config *Config) *Auth {
-    provider, err := oidc.NewProvider(ctx, config.Provider)
-    if err != nil {
-        log.Fatal(err)
-    }
-    auth := &Auth{
-        clientID: config.ClientID,
-        provider: provider,
-    }
-    http.HandleFunc(callback, auth.handle)
-    return auth
+	provider, err := oidc.NewProvider(ctx, config.Provider)
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := &Auth{
+		clientID: config.ClientID,
+		provider: provider,
+	}
+	http.HandleFunc(callback, auth.handle)
+	return auth
 }
 
 // Auth represents the auth module.
 type Auth struct {
-    clientID string
-    provider *oidc.Provider
+	clientID string
+	provider *oidc.Provider
 }
 
 const (
-    nonceCookie = "__Host-AuthNonce"
-    tokenCookie = "__Host-AuthToken"
+	nonceCookie = "__Host-AuthNonce"
+	tokenCookie = "__Host-AuthToken"
 )
 
 // Redirect redirects the user to the provider for authentication.
 func (s *Auth) Redirect(w http.ResponseWriter, r *http.Request) {
-    deleteCookie(w, tokenCookie)
-    nonce := hex.EncodeToString(randBytes(20))
-    const oneHour = 60 * 60
-    setCookie(w, nonceCookie, nonce, oneHour)
-    u := url.URL{
-        Scheme: "https",
-        Host:   r.Host,
-        Path:   callback,
-    }
-    v := url.Values{
-        "response_type": {"id_token"},
-        "client_id":     {s.clientID},
-        "redirect_uri":  {u.String()},
-        "scope":         {"email"},
-        "nonce":         {nonce},
-    }
-    authURL := s.provider.Endpoint().AuthURL
-    sep := "?"
-    if strings.Contains(authURL, "?") {
-        sep = "&"
-    }
-    http.Redirect(w, r, authURL+sep+v.Encode(), http.StatusFound)
+	deleteCookie(w, tokenCookie)
+	nonce := hex.EncodeToString(randBytes(20))
+	const oneHour = 60 * 60
+	setCookie(w, nonceCookie, nonce, oneHour)
+	u := url.URL{
+		Scheme: "https",
+		Host:   r.Host,
+		Path:   callback,
+	}
+	v := url.Values{
+		"response_type": {"id_token"},
+		"client_id":     {s.clientID},
+		"redirect_uri":  {u.String()},
+		"scope":         {"email"},
+		"nonce":         {nonce},
+	}
+	authURL := s.provider.Endpoint().AuthURL
+	sep := "?"
+	if strings.Contains(authURL, "?") {
+		sep = "&"
+	}
+	http.Redirect(w, r, authURL+sep+v.Encode(), http.StatusFound)
 }
 
 func (s *Auth) handle(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        fmt.Fprint(w, `<html><body><script>
+	if r.Method == "GET" {
+		fmt.Fprint(w, `<html><body><script>
 let hash = window.location.hash.substr(1);
 let fragments = hash.split('&').reduce((fragments, e) => {
     let parts = e.split('=');
@@ -139,80 +139,80 @@ form.appendChild(input);
 document.body.appendChild(form);
 form.submit();
 </script></body></html>`)
-        return
-    }
-    const skipExpiry = false
-    _, nonce, err := s.verify(r, r.FormValue("id_token"), skipExpiry)
-    if err != nil {
-        http.Error(w, "Invalid ID token: "+err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if c, err := r.Cookie(nonceCookie); err != nil || nonce != c.Value {
-        http.Error(w, "Invalid nonce", http.StatusInternalServerError)
-        return
-    }
-    deleteCookie(w, nonceCookie)
-    const oneYear = 365 * 24 * 60 * 60
-    setCookie(w, tokenCookie, r.FormValue("id_token"), oneYear)
-    http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	const skipExpiry = false
+	_, nonce, err := s.verify(r, r.FormValue("id_token"), skipExpiry)
+	if err != nil {
+		http.Error(w, "Invalid ID token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if c, err := r.Cookie(nonceCookie); err != nil || nonce != c.Value {
+		http.Error(w, "Invalid nonce", http.StatusInternalServerError)
+		return
+	}
+	deleteCookie(w, nonceCookie)
+	const oneYear = 365 * 24 * 60 * 60
+	setCookie(w, tokenCookie, r.FormValue("id_token"), oneYear)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 // User returns the user email after verifying the id token cookie.
 func (s *Auth) User(r *http.Request) (string, error) {
-    c, err := r.Cookie(tokenCookie)
-    if err != nil {
-        return "", fmt.Errorf("no auth token cookie")
-    }
-    const skipExpiry = true
-    email, _, err := s.verify(r, c.Value, skipExpiry)
-    if err != nil {
-        return "", fmt.Errorf("invalid ID token: %v", err)
-    }
-    return email, nil
+	c, err := r.Cookie(tokenCookie)
+	if err != nil {
+		return "", fmt.Errorf("no auth token cookie")
+	}
+	const skipExpiry = true
+	email, _, err := s.verify(r, c.Value, skipExpiry)
+	if err != nil {
+		return "", fmt.Errorf("invalid ID token: %v", err)
+	}
+	return email, nil
 }
 
 func (s *Auth) verify(r *http.Request, token string, skipExpiry bool) (string, string, error) {
-    config := &oidc.Config{ClientID: s.clientID}
-    if skipExpiry {
-        config.SkipExpiryCheck = true
-    }
-    idToken, err := s.provider.Verifier(config).Verify(r.Context(), token)
-    if err != nil {
-        return "", "", err
-    }
-    var claims struct {
-        Email         string `json:"email"`
-        EmailVerified bool   `json:"email_verified"`
-    }
-    if err := idToken.Claims(&claims); err != nil {
-        return "", "", fmt.Errorf("claims: %v", err)
-    }
-    if !claims.EmailVerified {
-        return "", "", fmt.Errorf("email not verified: %v", claims.Email)
-    }
-    return claims.Email, idToken.Nonce, nil
+	config := &oidc.Config{ClientID: s.clientID}
+	if skipExpiry {
+		config.SkipExpiryCheck = true
+	}
+	idToken, err := s.provider.Verifier(config).Verify(r.Context(), token)
+	if err != nil {
+		return "", "", err
+	}
+	var claims struct {
+		Email         string `json:"email"`
+		EmailVerified bool   `json:"email_verified"`
+	}
+	if err := idToken.Claims(&claims); err != nil {
+		return "", "", fmt.Errorf("claims: %v", err)
+	}
+	if !claims.EmailVerified {
+		return "", "", fmt.Errorf("email not verified: %v", claims.Email)
+	}
+	return claims.Email, idToken.Nonce, nil
 }
 
 func setCookie(w http.ResponseWriter, name, value string, maxAge int) {
-    http.SetCookie(w, &http.Cookie{
-        Name:     name,
-        Value:    value,
-        Path:     "/",
-        MaxAge:   maxAge,
-        Secure:   true,
-        HttpOnly: true,
-        SameSite: http.SameSiteStrictMode,
-    })
+	http.SetCookie(w, &http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		MaxAge:   maxAge,
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	})
 }
 
 func deleteCookie(w http.ResponseWriter, name string) {
-    setCookie(w, name, "", -1)
+	setCookie(w, name, "", -1)
 }
 
 func randBytes(length int) []byte {
-    b := make([]byte, length)
-    if _, err := rand.Read(b); err != nil {
-        panic(fmt.Sprintf("read rand failed: %v", err))
-    }
-    return b
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		panic(fmt.Sprintf("read rand failed: %v", err))
+	}
+	return b
 }
